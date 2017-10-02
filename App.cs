e7d04460a45fdee9a1eb3909c108a030d541729f
RhysUCP <Rhys.Sharpe.PRC@Gmail.com>
2017-10-02 15:08:28 +0100

    Added App class
    Created the default App class and Run method

---
+++ App.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Shapes
+{
+    class App
+    {
+        public App()
+        {
+            
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello Rhys");
+        }
+
+    }
+}
