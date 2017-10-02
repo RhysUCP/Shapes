61891648dd43c98aa5cf281e645604ca785502ec
RhysUCP <Rhys.Sharpe.PRC@Gmail.com>
2017-10-02 16:24:21 +0100

    Added properties to Triangle
    Added Accessor and Mutators for Height and BaseLength

--- App.cs
+++ App.cs
@@ -5,13 +5,19 @@ namespace Shapes
     class App
     {
         public App()
-        {
-            
-        }
+        {}
 
         public void Run()
         {
-            Console.WriteLine("Hello Rhys");
+            Triangle triangleOne = new Triangle();
+
+            Console.WriteLine("Enter the height of the triangle: ");
+            triangleOne.Height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the base length of the triangle: ");
+            triangleOne.BaseLength = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("The area of the triangle is " + triangleOne.GetArea());
         }
 
     }
--- Triangle.cs
+++ Triangle.cs
@@ -6,15 +6,44 @@ namespace Shapes
     {
         private double height, baseLength;
 
-        public Triangle(double height, double baseLength)
-        {
-            this.height = height;
-            this.baseLength = baseLength;
-        }
+        public Triangle()
+        {}
 
         public double GetArea()
         {
             return height * baseLength * 0.5D;
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                if (value < 0 || value > double.MaxValue)
+                    throw new OverflowException();
+
+                height = value;
+            }
+        }
+
+        public double BaseLength
+        {
+            get
+            {
+                return baseLength;
+            }
+
+            set
+            {
+                if (value < 0 || value > double.MaxValue)
+                    throw new OverflowException();
+
+                baseLength = value;
+            }
         }
 
     }
